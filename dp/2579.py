2304a469dfb578518bfa1c9d7d62bc2b45c5fce1
kyuhwanpark <parkkh9989@naver.com>
2024-03-16 16:27:57 +0900

    add file

---
+++ dp/2579.py
@@ -0,0 +1,15 @@
+import sys
+N = int(sys.stdin.readline())
+stair = [0]*(301)
+for i in range(N):
+    stair[i]=int(sys.stdin.readline())
+
+DP = [0]*(301)
+DP[0] = stair[0]
+DP[1] = stair[0]+stair[1]
+DP[2] = max(stair[0]+stair[2], stair[1]+stair[2])
+
+for i in range(3, N):
+    DP[i] = max(DP[i-3] + stair[i-1] + stair[i], DP[i-2]+stair[i])
+
+print(DP[N-1])
